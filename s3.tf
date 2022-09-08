2de968c07c8a8c371cd61183b4a2ce79f9c82f6f
Daniel Penalva <dkajah@gmail.com>
2022-09-08 12:46:02 -0300

    correcting typo deleting main.tf

--- s3.tf
+++ s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "datalake" {
     server_side_encryption_configuration {
         rule {
             apply_server_side_encryption_by_default{
-                sse_algorithm = "AES255"
+                sse_algorithm = "AES256"
             }
         }
     }
